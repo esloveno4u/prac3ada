97f27adf83bafb026e5303cdc0e37f0bbed63016
esloveno4u <slavv64@gmail.com>
2024-11-12 00:40:54 +0100

    tarifa medio bien medio mal

--- src/TarifaTelefonica.java
+++ src/TarifaTelefonica.java
@@ -15,19 +15,40 @@ public class TarifaTelefonica {
 	}
 
 	public int resolverBottomUp() {
-
+			int [] pago = new int[estimacion.length];
 		//***Completar Implementación***
-
-		return pago[estimacion.length-1];
+		for(int i = estimacion.length-1; i>=0;i--){
+			if(i==estimacion.length-1){
+				pago[i]=tarifaMegas * estimacion[i];
+			}else if(i+permanencia>estimacion.length-1) {
+				pago[i] = pago[i + 1] + tarifaMegas * estimacion[i];
+			}else{
+				if(pago[i + 1] + tarifaMegas * estimacion[i]<tarifaPlana*permanencia+pago[i+permanencia]){
+					pago[i] = pago[i + 1] + tarifaMegas * estimacion[i];
+				}else{
+					pago[i]=tarifaPlana*permanencia+pago[i+permanencia];
+				}
+			}
+		}
+		return pago[0];
 	}
 
 	public int[] reconstruirSol() {
 		if (pago == null) {
 			throw new RuntimeException("Se debe resolver el problema primero");
 		}
-		
-		//***Completar implementación***
-		return null;
+		int [] sol= new int[pago.length];
+		for(int i = 0 ; i<pago.length;i++){
+			if(i+permanencia>estimacion.length-1){
+				sol[i]=0;
+			}
+			else if(pago[i]==pago[i+1]+tarifaMegas * estimacion[i]){
+				sol[i]=0;
+			}else{
+				sol[i]=1;
+			}
+		}
+		return sol;
 	}
 
 	public void imprimeVectorSolucion() {
