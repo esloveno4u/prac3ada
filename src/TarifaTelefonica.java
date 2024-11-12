e0b66166f42f86f674d33cd712dd917609f1f60b
esloveno4u <slavv64@gmail.com>
2024-11-12 01:19:14 +0100

    proyeccion de mi optimismo

--- src/TarifaTelefonica.java
+++ src/TarifaTelefonica.java
@@ -15,18 +15,20 @@ public class TarifaTelefonica {
 	}
 
 	public int resolverBottomUp() {
-			int [] pago = new int[estimacion.length];
+			pago = new int[estimacion.length];
 		//***Completar Implementación***
 		for(int i = estimacion.length-1; i>=0;i--){
 			if(i==estimacion.length-1){
 				pago[i]=tarifaMegas * estimacion[i];
-			}else if(i+permanencia>estimacion.length-1) {
+			}else if(i+permanencia>estimacion.length-1){
 				pago[i] = pago[i + 1] + tarifaMegas * estimacion[i];
 			}else{
 				if(pago[i + 1] + tarifaMegas * estimacion[i]<tarifaPlana*permanencia+pago[i+permanencia]){
 					pago[i] = pago[i + 1] + tarifaMegas * estimacion[i];
 				}else{
-					pago[i]=tarifaPlana*permanencia+pago[i+permanencia];
+					for(int j=i+permanencia-1; j>=i;j--){
+						pago[j]=tarifaPlana+pago[j+1];
+					}
 				}
 			}
 		}
@@ -38,11 +40,8 @@ public class TarifaTelefonica {
 			throw new RuntimeException("Se debe resolver el problema primero");
 		}
 		int [] sol= new int[pago.length];
-		for(int i = 0 ; i<pago.length;i++){
-			if(i+permanencia>estimacion.length-1){
-				sol[i]=0;
-			}
-			else if(pago[i]==pago[i+1]+tarifaMegas * estimacion[i]){
+		for(int i = 0 ; i<pago.length-1;i++){
+			if(pago[i]==pago[i+1]+tarifaMegas * estimacion[i]){
 				sol[i]=0;
 			}else{
 				sol[i]=1;
